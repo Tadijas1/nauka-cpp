38bd8215e5fb4a74ebaf4f64fbc19894f5e2102f
Tadijas <ctadeusz14@gmail.com>
2025-06-23 20:44:35 +0200

    film 15

---
+++ film15.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+string napis;
+
+int main()
+{
+    cin>>napis;
+
+    cout<<"Wpisany ciąg znaków: "<<napis<<endl; //W LINUXIE DZIAŁA W W WINDOWSIE NIE!!!
+    
+    return 0;
+}
