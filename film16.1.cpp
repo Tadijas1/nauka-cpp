977b5dd632a3b0cd1de978183af0ecd9d3be22db
Tadijas <ctadeusz14@gmail.com>
2025-06-28 22:10:29 +0200

    FILM 16

---
+++ film16.1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    /*    STOS    do palenia żydów*/
+    cout<<"Hellow world!"<<endl;
+    /*    KOLEJKA    */
+    cout<<"Hellow world!"<<endl;
+    /*    LISTA    */
+    cout<<"Hellow world!"<<endl;
+    /*    DRZEWO    */
+    cout<<"Hellow world!"<<endl;
+
+    return 0; 
+}
