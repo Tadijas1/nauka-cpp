071bd6e2fd3a02221f4190d8f7195a11419fd75d
Tadijas1 <ctadeusz14@gmail.com>
2025-07-18 09:56:59 +0200

    obiektowy film 5

--- ofilm5/main.cpp
+++ ofilm5/main.cpp
@@ -23,8 +23,10 @@ class Punkt
 
 class Kolo :public Punkt // klasa Kolo dziedziczy publicznie z klasy Punkt
 {
-    float r;
     string nazwa;
+    
+    protected:
+    float r;
 
     public:
     void wyswietl()
@@ -43,10 +45,27 @@ class Kolo :public Punkt // klasa Kolo dziedziczy publicznie z klasy Punkt
     }
 };
 
+class Kula :public Kolo
+{
+    string nazwa;
+
+    public:
+    Kula(string nkul="Kula", string nk="Kółko", string np="S", float a=0, float b=0, float pr=1)
+    :Kolo(nk, np, a, b, pr)
+    {
+        nazwa=nkul;
+    }
+    void wyswietl()
+    {
+        Kolo::wyswietl();
+        cout<<"Obiętość kuli: "<<(4/3)*M_PI*r*r*r<<endl;
+    }
+};
+
 int main()
 {
-    Kolo k1;
-    k1.wyswietl();
+    Kula kul1;
+    kul1.wyswietl();
     
     return 0;
 }
